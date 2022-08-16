0082a7c47447d07702005292144da80f2612f03c
Cbutler1 <collin.p.butler1@gmail.com>
2022-08-15 22:16:54 -0400

    addnewmember method

--- MidtermProject/Club.cs
+++ MidtermProject/Club.cs
@@ -8,13 +8,26 @@ namespace MidtermProject
 {
     public class Club : Members
     {
-        public Club(int id, string name, string address) : base (id, address)
+        public Club(int id, string name, string address) : base(id, address)
         {
+            Id = id;
             Name = name;
             Address = address;
         }
-
+        //possibly add club to these to seperate from members
         public string Name { get; set; }
         public string Address { get; set; }
+
+     
+        
+        public Dictionary<string, string> userMembers = new Dictionary<string, string>();
+        public void AddMembers(string name, string address)
+        {
+
+            userMembers.Add(name, address);
+
+
+          
+        }
     }
 }
