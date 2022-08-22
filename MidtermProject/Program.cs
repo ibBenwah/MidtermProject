bf04cc830f083ffa1beba3a4ae12e84e2fe1d093
Cbutler1 <collin.p.butler1@gmail.com>
2022-08-22 15:26:40 -0400

    updated program file

--- MidtermProject/Program.cs
+++ MidtermProject/Program.cs
@@ -13,7 +13,35 @@ SingleMember.CheckIn();
 SingleMember.AddMember();
 //SingleMember.MemberInfo();
 
+bool continueprogram = true;
 
+Console.WriteLine("Welcome to the Fitness Center!");
+while(continueprogram = true)
+{
+    Console.WriteLine("Hello!  1:Join New Club 2:Check In 3:Add/Remove Members 4:Display Member Information 5: Pay Bills");
+    int userSelection = int.Parse(Console.ReadLine());
+    if (userSelection == 1)
+    {
+        //join club
+    }
+    else if (userSelection == 2)
+    {
+        //checkin
+    }
+    else if (userSelection == 3)
+    {
+        //add remove members based on another read line
+    }
+    else if (userSelection == 4)
+    {
+        //display member info
+    }
+    else if (userSelection == 5)
+    {
+        //pay bills
+    }
+    else { Console.WriteLine("Please enter a valid entry"); }
+}
 
 
 
